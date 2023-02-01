80b2bb6f7999e3575092223eb9a27dbb72d93553
WILLINGTON RENTERIA <wjrenteria@gmail.com>
2023-02-01 10:28:54 -0800

    Update test.py

--- test.py
+++ test.py
@@ -6,9 +6,12 @@ Created on Wed Feb  1 09:51:06 2023
 """
 import numpy as np
 import streamlit as st
+f = open('texto.txt')
+t = f.readline()
+f.close()
 st.write("Hello ,let's learn how to build a streamlit app together")
 st.title ("Test application")
-st.header("this is the markdown")
+st.header(t)
 st.markdown("this is the header")
 st.subheader("this is the subheader")
 st.caption("this is the caption")
