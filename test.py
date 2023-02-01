82263dbab188cc048230c4da767b4f4e73a56f7f
WILLINGTON RENTERIA <wjrenteria@gmail.com>
2023-02-01 09:58:26 -0800

    test
    test

---
+++ test.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  1 09:51:06 2023
+
+@author: wrenteria
+"""
+
+import streamlit as st
+st.write("Hello ,let's learn how to build a streamlit app together")
+st.title ("this is the app title")
+st.header("this is the markdown")
+st.markdown("this is the header")
+st.subheader("this is the subheader")
+st.caption("this is the caption")
+st.code("x=2021")
+st.latex(r''' a+a r^1+a r^2+a r^3 ''')
